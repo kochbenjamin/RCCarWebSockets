92cb31e83afab7ec8a60976d2ced80b3d6ae7f82
Benjamin.Koch <benjamin.koch.my8@fh-zwickau.de>
2025-08-27 12:45:19 +0200

    Projektdateien hinzufügen.

---
+++ RCCarWebSockets/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Device.Gpio;
+using System.Threading.Tasks;
+
+const int Pin = 21;
+const string Alert = "ALERT 🚨";
+const string Ready = "READY ✅";
+
+using var controller = new GpioController();
+controller.OpenPin(Pin, PinMode.InputPullUp);
+
+Console.WriteLine(
+    $"Initial status ({DateTime.Now}): {(controller.Read(Pin) == PinValue.High ? Alert : Ready)}");
+
+controller.RegisterCallbackForPinValueChangedEvent(
+    Pin,
+    PinEventTypes.Falling | PinEventTypes.Rising,
+    OnPinEvent);
+
+await Task.Delay(Timeout.Infinite);
+
+static void OnPinEvent(object sender, PinValueChangedEventArgs args)
+{
+    Console.WriteLine(
+        $"({DateTime.Now}) {(args.ChangeType is PinEventTypes.Rising ? Alert : Ready)}");
+}
