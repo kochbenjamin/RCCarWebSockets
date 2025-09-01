e612450fe08781e65426d90c6b01291cc49e3f3c
Benjamin.Koch <benjamin.koch.my8@fh-zwickau.de>
2025-09-01 16:07:53 +0200

    WebSocket test

--- RCCarWebSockets/Program.cs
+++ RCCarWebSockets/Program.cs
@@ -1,8 +1,66 @@
-﻿using RCCarWebSockets;
-using System.Device.Gpio;
-using System.Threading.Tasks;
+﻿using System.Net.WebSockets;
+using System.Text;
+using System.Text.Json;
 
-var controller = new MotorController();
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseWebSockets();
+
+app.Map("/ws", async context =>
+{
+    if (context.WebSockets.IsWebSocketRequest)
+    {
+        using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+        Console.WriteLine("WebSocket connected");
+
+        var buffer = new byte[1024 * 4];
+
+        while (true)
+        {
+            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            if (result.MessageType == WebSocketMessageType.Close)
+            {
+                Console.WriteLine("WebSocket closed");
+                break;
+            }
+
+            var jsonString = Encoding.UTF8.GetString(buffer, 0, result.Count);
+            Console.WriteLine("Received JSON: " + jsonString);
+
+            try
+            {
+                var command = JsonSerializer.Deserialize<ControlCommand>(jsonString);
+                if (command != null)
+                {
+                    Console.WriteLine($"Drive: {command.Drive}, Speed: {command.Direction}");
+
+                    // TODO: Steuerung des Autos anstoßen
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid JSON: " + ex.Message);
+            }
+        }
+    }
+    else
+    {
+        context.Response.StatusCode = 400;
+    }
+});
+
+app.Run("http://0.0.0.0:5000");
+
+
+public class ControlCommand
+{
+    public string Direction { get; set; } = "";
+    public int Drive { get; set; }
+}
+
+
+/*var controller = new MotorController();
 
 
 
@@ -24,4 +82,4 @@ controller.DriveBackWard();
 
 await Task.Delay(500);
 
-controller.DriveStop();
+controller.DriveStop();*/
