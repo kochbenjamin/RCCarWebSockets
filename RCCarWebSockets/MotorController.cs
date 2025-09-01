0727d7e6c60b678a5a20a8a8be51b8f3b3e922d3
Benjamin.Koch <benjamin.koch.my8@fh-zwickau.de>
2025-09-01 15:15:14 +0200

    Motorontroller

---
+++ RCCarWebSockets/MotorController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Device.Gpio;
+
+namespace RCCarWebSockets
+{
+    internal class MotorController
+    {
+        private GpioController controller = new GpioController();
+
+        public MotorController() 
+        {
+            controller.OpenPin(17, PinMode.Output);
+            controller.OpenPin(27, PinMode.Output);
+
+            controller.OpenPin(22, PinMode.Output);
+            controller.OpenPin(23, PinMode.Output);
+        }
+
+        public void DriveForward()
+        {
+            Console.WriteLine("Antrieb vorwärts");
+            controller.Write(22, PinValue.High);
+            controller.Write(23, PinValue.Low);
+        }
+
+        public void DriveBackWard()
+        {
+            Console.WriteLine("Antrieb rückwärts");
+            controller.Write(22, PinValue.Low);
+            controller.Write(23, PinValue.High);
+        }
+
+        public void DriveStop()
+        {
+            Console.WriteLine("Antrieb stop");
+            controller.Write(22, PinValue.Low);
+            controller.Write(23, PinValue.Low);
+        }
+
+        public void TurnLeft()
+        {
+            Console.WriteLine("Lenkung links");
+            controller.Write(17, PinValue.High);
+            controller.Write(27, PinValue.Low);
+        }
+
+        public void TurnRight()
+        {
+            Console.WriteLine("Lenkung rechts");
+            controller.Write(17, PinValue.Low);
+            controller.Write(27, PinValue.High);
+        }
+
+        public void TurnStop()
+        {
+            Console.WriteLine("Lenkung stop");
+            controller.Write(17, PinValue.Low);
+            controller.Write(27, PinValue.Low);
+        }
+
+        ~MotorController()
+        {
+            controller.ClosePin(17);
+            controller.ClosePin(27);
+            controller.ClosePin(22);
+            controller.ClosePin(23);
+        }
+    }
+}
--- RCCarWebSockets/Program.cs
+++ RCCarWebSockets/Program.cs
@@ -1,42 +1,27 @@
-﻿using System.Device.Gpio;
+﻿using RCCarWebSockets;
+using System.Device.Gpio;
 using System.Threading.Tasks;
 
-const int Pin = 17;
+var controller = new MotorController();
 
 
-using var controller = new GpioController();
-controller.OpenPin(Pin, PinMode.Output);
-controller.OpenPin(27, PinMode.Output);
 
-controller.OpenPin(22, PinMode.Output);
-controller.OpenPin(23, PinMode.Output);
-
-Console.WriteLine("lenkung Zustand 1");
-controller.Write(17, PinValue.High);
-controller.Write(27, PinValue.Low);
+controller.TurnLeft();
 
 await Task.Delay(500); // 0,5 Sekunden warten
 
-Console.WriteLine("lenkung Zustand 2");
-controller.Write(17, PinValue.Low);
-controller.Write(27, PinValue.High);
+controller.TurnRight();
 
 await Task.Delay(500);
 
-controller.Write(17, PinValue.Low);
-controller.Write(27, PinValue.Low);
+//controller.TurnStop();
 
-Console.WriteLine("Antrieb Zustand 1");
-controller.Write(22, PinValue.High);
-controller.Write(23, PinValue.Low);
+controller.DriveForward();
 
 await Task.Delay(500);
 
-Console.WriteLine("Antrieb Zustand 2");
-controller.Write(22, PinValue.Low);
-controller.Write(23, PinValue.High);
+controller.DriveBackWard();
 
 await Task.Delay(500);
 
-controller.Write(22, PinValue.Low);
-controller.Write(23, PinValue.Low);
+controller.DriveStop();
