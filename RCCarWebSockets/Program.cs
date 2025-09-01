c095a8bffcd4ee154b67db1ee157b8e1f4b8fb41
Benjamin.Koch <benjamin.koch.my8@fh-zwickau.de>
2025-09-01 13:53:43 +0200

    Motoren  komplett Test

--- RCCarWebSockets/Program.cs
+++ RCCarWebSockets/Program.cs
@@ -8,16 +8,35 @@ using var controller = new GpioController();
 controller.OpenPin(Pin, PinMode.Output);
 controller.OpenPin(27, PinMode.Output);
 
+controller.OpenPin(22, PinMode.Output);
+controller.OpenPin(23, PinMode.Output);
+
+Console.WriteLine("lenkung Zustand 1");
 controller.Write(17, PinValue.High);
 controller.Write(27, PinValue.Low);
 
-await Task.Delay(1000); // 1 Sekunde warten
+await Task.Delay(500); // 0,5 Sekunden warten
+
+Console.WriteLine("lenkung Zustand 2");
+controller.Write(17, PinValue.Low);
+controller.Write(27, PinValue.High);
+
+await Task.Delay(500);
 
 controller.Write(17, PinValue.Low);
 controller.Write(27, PinValue.Low);
 
+Console.WriteLine("Antrieb Zustand 1");
+controller.Write(22, PinValue.High);
+controller.Write(23, PinValue.Low);
 
-Console.WriteLine("Motor gestoppt.");
+await Task.Delay(500);
 
-controller.Write(17, PinValue.Low);
-controller.Write(27, PinValue.Low);
+Console.WriteLine("Antrieb Zustand 2");
+controller.Write(22, PinValue.Low);
+controller.Write(23, PinValue.High);
+
+await Task.Delay(500);
+
+controller.Write(22, PinValue.Low);
+controller.Write(23, PinValue.Low);
